73dbd1aa679aea3e75586138ace22886dad3b574
JaWarrior12 <jacob.meier@cox.net>
2024-02-04 11:39:02 -0500

    Added capability for new leaderboards; updated readME; fixed toml again

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dredarkLeaderboardLib
-Dredark Leaderboard Library for Python!
+Dredark Leaderboard Library for Python!\n
 The library contains functions to fetch data from the Deep Space Airships (drednot.io) leaderboard pages.
+
+Currently in Pre-Release
--- src/dredarkLeaderboardLib_JaWarrior12/base.py
+++ src/dredarkLeaderboardLib_JaWarrior12/base.py
@@ -19,14 +19,42 @@ class Leaderboard():
         Returns:
             dict: Dictonary of ships, the key is the hexcode followed by a dict containing name (str), rank (str), and score (str).
         """
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        ships = soup.find("tbody").find_all("tr",limit=limit)
         ship_info={}
-        for ship in ships:
-            rawData=ship.find_all("td")
-            data=[i.text.strip() for i in rawData]
-            hexcode=data[2].replace("{","").replace("}","")
-            ship_info.update({hexcode:{"name":data[1],"rank":data[0],"score":data[3].split(" ")[0]}})
-        print(ship_info)
+        if "ship" in url:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            ships = soup.find("tbody").find_all("tr",limit=limit)
+            for ship in ships:
+                rawData=ship.find_all("td")
+                data=[i.text.strip() for i in rawData]
+                hexcode=data[2].replace("{","").replace("}","")
+                ship_info.update({hexcode:{"name":data[1],"rank":data[0],"score":data[3].split(" ")[0]}})
+            #print(ship_info)
+        elif "pilot" in url:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            ships = soup.find("tbody").find_all("tr",limit=limit)
+            for ship in ships:
+                rawData=ship.find_all("td")
+                data=[i.text.strip() for i in rawData]
+                name=data[1]
+                ship_info.update({name:{"rank":data[0],"score":data[3].split(" ")[0]}})
+        elif "clan" in url:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            ships = soup.find("tbody").find_all("tr",limit=limit)
+            for ship in ships:
+                rawData=ship.find_all("td")
+                data=[i.text.strip() for i in rawData]
+                clan=data[1]
+                ship_info.update({clan:{"rank":data[0],"score":data[3].split(" ")[0]}})
+        else:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            ships = soup.find("tbody").find_all("tr",limit=limit)
+            for ship in ships:
+                rawData=ship.find_all("td")
+                data=[i.text.strip() for i in rawData]
+                hexcode=data[2].replace("{","").replace("}","")
+                ship_info.update({hexcode:{"name":data[1],"rank":data[0],"score":data[3].split(" ")[0]}})
         return ship_info
