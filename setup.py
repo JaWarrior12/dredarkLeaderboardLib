15cd892041cfe324f1f302b57c6b4c03afe84c39
JaWarrior12 <jacob.meier@cox.net>
2024-02-04 10:32:00 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup
 
-setup(
-   name='dredarkLeaderboardLib',
-   version='1.0.0-alpha',
-   description='A Library to fetch from the Deep Space Airships leaderboards.',
-   author='JaWarrior12',
-   packages=['dredarkLeaderboardLib'],  #same as name
-   install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
-)
+if __name__ == "__main__":
+    setuptools.setup()
