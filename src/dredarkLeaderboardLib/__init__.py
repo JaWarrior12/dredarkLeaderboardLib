293ebf5daa2d364a0c9a40ef591788bd6f759151
JaWarrior12 <jacob.meier@cox.net>
2024-02-07 19:01:41 -0500

    Fixed Errors not registering?

--- src/dredarkLeaderboardLib/__init__.py
+++ src/dredarkLeaderboardLib/__init__.py
@@ -1,2 +1,2 @@
-from .base import *
 from .errors import *
+from .base import *
--- src/dredarkLeaderboardLib/errors.py
+++ src/dredarkLeaderboardLib/errors.py
@@ -1,5 +1,14 @@
-__all__=("BadSearchKey")
+__all__=('BadSearchKey')
+
+class customError(Exception):
+    pass
 
 class BadSearchKey(Exception):
     """Bad Search Key; Key Not In Usable Keys List"""
-    pass
+    def __init__(self):
+        self.value="BadSearchKey"
+        #self.arg1 = searchTerms
+        #self.arg2 = viableKeys
+        #super(BadSearchKey, self).__init__(message)
+    def __str__(self):
+        return repr(self.value)
