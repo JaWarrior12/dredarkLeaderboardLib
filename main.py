37d6b65276bdc10b1653a632322de1bb8af0167b
JaWarrior12 <97358009+JaWarrior12@users.noreply.github.com>
2024-04-10 19:20:37 +0000

    Changes To pyproject.toml
    Added Language Classifier
    Added Dependencies List
    Added License
    Added Keywords
    Added Wiki Link

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ statistics=dll.Leaderboard()
 statistics.scan_Leaderboard(URL,LIMIT)
 #print(statistics.shipData)
 #Or
-#print(statistics.return_data())
+print(statistics.return_data())
 print(statistics.fetch_ship("name","JaMWarrior"))
 print(statistics.fetch_ranks(1,10,True,False))
 #print(statistics.bs4Soup)
